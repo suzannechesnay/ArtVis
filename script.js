1508a0a019d4ea9b4fcf5edc3024a1e58b49f4e6
emilindominic <63548538+emilindominic@users.noreply.github.com>
2025-01-18 21:24:48 +0100

    misc

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let heatmapData = [];
 let exhibitions = [];
 let artists = [];
 let venues = [];
@@ -120,75 +119,6 @@ async function fetchMapData(startYear, endYear) {
   }
 }
 
-
-/*
-async function loadData() {
-  const [exhibitions, artists] = await Promise.all([
-    axios.get("../data/exhibition_data.js"),
-  ]);
-
-  processData(exhibitions.data);
-}
-
-function processData(e) {
-  venues = rawVenues.map((e) => ({
-    e_id: e_id,
-    e_title: e_title,
-    venue: e_venue,
-    paintings: e_paintings,
-    artistId: a_id,
-  }));
-
-  exhibitions = rawExhibitions.map((e) => ({
-    id: e_id,
-    title: e_title,
-    venue: e_venue,
-    startDate: new Date(e_startDate),
-    type: e_type,
-    paintings: e_paintings,
-    country: e_country,
-    city: e_city,
-    lat: e_latitude,
-    lng: e_longitude,
-    artistId: a_id,
-  }));
-
-  const ExhibitionsMap = new Map(
-    exhibitions.map((e) => [e_id, exhibition])
-  );
-
-  exhibitions.forEach((e) => {
-    const exhibition = ExhibitionsMap.get(artistId);
-    if (artistsMap.has(exhibition.a_id)) {
-      artist.totalPaintings += exhibition.e_paintings;
-      artist.exhibitions.add(exhibition.e_id);
-      artist.venues.add(exhibition.e_venue);
-    }
-  });
-
-  //createHeatmapData();
-  updateVisualization();
-}
-
-loadData();*/
-
-/*function createHeatmapData() {
-  const venueMap = new Map();
-
-  exhibitions.forEach((exhibition) => {
-    const key = `${exhibition.venue}`;
-    if (!venueMap.has(key)) {
-      venueMap.set(key, {
-        lat: exhibition.lat,
-        lng: exhibition.lng,
-        count: 0,
-      });
-    }
-    venueMap.get(key).count++;
-  });
-
-  heatmapData = Array.from(venueMap.values());
-}*/
 
 function updateVisualization() {
   updateHeatmap();
